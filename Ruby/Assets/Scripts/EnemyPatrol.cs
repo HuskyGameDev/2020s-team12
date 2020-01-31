2d861c9cdb4e009edea02a4ddd3aa51b31a34647
Jolo Vandenberg <jolov@mtu.edu>
2020-01-31 16:56:08 -0500

    Finished PatrolPoints Script
    - The enemy can now patrol between two points while Ruby is not within its detection radius

--- Ruby/Assets/Scripts/EnemyPatrol.cs
+++ Ruby/Assets/Scripts/EnemyPatrol.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    public float patrolMoveVelocity = 0.02f; // Movement speed while the enemy is patrolling
+    public float patrolMoveVelocity = .02f; // Movement speed while the enemy is patrolling
     Vector3 movement; // Movement vector
     public float point1X = 0; // The X coordinate of the desired first patrol point
     public float point1Y = 0; // The Y coordinate of the first patrol point
     public float point2X = 0; // The X coordinate of the second patrol point
     public float point2Y = 0; // The Y coordinate of the second control point
-    bool movingTowardsPoint1 = true; // Whether the enemy should be heading to point 1 or 2.
+    public float atPointWaitTime = 1f; // How long the enemy waits upon reaching one of the patrol points
+    bool movingTowardsPoint1 = true; // Whether the enemy should be heading to point 1 or 2
 
-    float tolerance = 0.5f;
+    float tolerance = .01f; // How close the enemy can be to the point for it to be called at the point
 
     // Start is called before the first frame update
     void Start()
@@ -20,41 +21,38 @@ public class EnemyPatrol : MonoBehaviour
         
     }
 
-    public void PatrolPoints()
+    public void PatrolPoints() // Makes the enemy patrol between the points
     {
         if (movingTowardsPoint1)
         {
-            float moveX = point1X - transform.position.x;
-            float moveY = point1Y - transform.position.x;
-
-            movement = new Vector3(moveX, moveY, 0);
-
-            movement.Normalize();
-
-            transform.position += (patrolMoveVelocity * movement);
-
-            if (Mathf.Abs(moveX) <= tolerance && Mathf.Abs(moveY) <= tolerance)
-            {
-                print("Switcheroo!");
-                movingTowardsPoint1 = false;
-            }
+            StartCoroutine(MoveTowardsPoint(point1X,point1Y));
         }
-        else
+        else 
         {
-            float moveX = point2X - transform.position.x;
-            float moveY = point2Y - transform.position.x;
-
-            movement = new Vector3(moveX, moveY, 0);
-
-            movement.Normalize();
-
-            transform.position += (patrolMoveVelocity * movement);
-
-            if (Mathf.Abs(moveX) <= tolerance && Mathf.Abs(moveY) <= tolerance)
-            {
-                movingTowardsPoint1 = true;
-            }
+            StartCoroutine(MoveTowardsPoint(point2X, point2Y));
         }
+    }
+
+    IEnumerator MoveTowardsPoint(float xCoord, float yCoord)
+    {
+
+        float moveX = xCoord - transform.position.x; // Calculate movement vector based on current position and the point
+        float moveY = yCoord - transform.position.y;
+
+        movement = new Vector3(moveX, moveY, 0); // Set values for movement vector
+
+        movement.Normalize(); // Normalize vector
+
+        if (Mathf.Abs(moveX) <= tolerance && Mathf.Abs(moveY) <= tolerance) // Once enemy reaches the desired patrol point within a given tolerance
+        {
+            movingTowardsPoint1 = !movingTowardsPoint1; // Start heading towards the other point
+            float velocityHolder = patrolMoveVelocity;
+            patrolMoveVelocity = 0; // Stop moving
+            yield return new WaitForSeconds(atPointWaitTime); // Wait for a little bit based on the atPointWaitTime variable
+            patrolMoveVelocity = velocityHolder; // Start moving again
+        }
+
+        transform.position += (patrolMoveVelocity * movement); // Moves the enemy
     }
 
     // Update is called once per frame
