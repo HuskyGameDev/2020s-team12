27a1e08bb405e4f56a95ef90d8523b9ae8befd65
Jessie Moore <jessejm123@gmail.com>
2020-02-25 19:00:48 -0500

    Room Transitions (+Tweaks to UI)
    - Added "Main Camera Controller" Prefab; use this in place of the "Main Camera" Prefab
    - Updated The UI Prefab

---
+++ Ruby/Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CameraController : MonoBehaviour
+{
+    public const float roomWidth = 30f;
+    public const float roomHeight = 20f;
+    public Transform player; // Does not need to be set in editor
+
+    Animator anim;
+    int[] currentRoom = { 0, 0 }; // Room coords X and Y
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player == null)
+            player = GameObject.Find("Ruby").transform; // Maybe change this later??
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = player.position - transform.position;
+
+        if(relativePos.x > (roomWidth / 2f)) // Player moving to right room
+        {
+            transform.position += (Vector3.right * roomWidth);
+            anim.SetTrigger("MoveRight");
+        }
+        else if(relativePos.x < -(roomWidth / 2f))
+        {
+            transform.position += (Vector3.left * roomWidth);
+            anim.SetTrigger("MoveLeft");
+        }
+        else if (relativePos.y > (roomHeight / 2f - 2.5f))
+        {
+            transform.position += (Vector3.up * roomHeight);
+            anim.SetTrigger("MoveUp");
+        }
+        else if (relativePos.y < -(roomHeight / 2f + 2.5f))
+        {
+            transform.position += (Vector3.down * roomHeight);
+            anim.SetTrigger("MoveDown");
+        }
+    }
+}
