b7a4b237c0cffe98c6519deeaba8e0a52b8a1ced
Jolo Vandenberg <jolov@mtu.edu>
2020-02-27 13:47:17 -0500

    trying to fix patrol
    - enemies should be able to have a single point patrol

--- Ruby/Assets/Scripts/EnemyPatrol.cs
+++ Ruby/Assets/Scripts/EnemyPatrol.cs
@@ -10,6 +10,9 @@ public class EnemyPatrol : MonoBehaviour
     public float relativePoint1Y = 0; // The relative Y coordinate of the first patrol point
     public float relativePoint2X = 0; // The relative X coordinate of the second patrol point
     public float relativePoint2Y = 0; // The relative Y coordinate of the second patrol point
+
+    public bool onePointPatrol = false;
+
     float point1X = 0; // The actual coordinates of the first patrol point
     float point1Y = 0;
     float point2X = 0; // The actual coordinates of the second patrol point
@@ -19,7 +22,7 @@ public class EnemyPatrol : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
 
-    float tolerance = .05f; // How close the enemy can be to the point for it to be called at the point
+    float tolerance = .1f; // How close the enemy can be to the point for it to be called at the point
 
     // Start is called before the first frame update
     void Start()
@@ -37,42 +40,51 @@ public class EnemyPatrol : MonoBehaviour
 
     public void PatrolPoints() // Makes the enemy patrol between the points
     {
-        if (movingTowardsPoint1)
+        if (onePointPatrol)
         {
-            StartCoroutine(MoveTowardsPoint(point1X,point1Y));
+            StartCoroutine(MoveTowardsPoint(point1X, point1Y));
         }
-        else 
+        else
         {
-            StartCoroutine(MoveTowardsPoint(point2X, point2Y));
+            if (movingTowardsPoint1)
+            {
+                StartCoroutine(MoveTowardsPoint(point1X, point1Y));
+            }
+            else
+            {
+                StartCoroutine(MoveTowardsPoint(point2X, point2Y));
+            }
         }
     }
 
     IEnumerator MoveTowardsPoint(float xCoord, float yCoord)
     {
-        if (patrolMoveVelocity != 0)
+        float moveX = xCoord - transform.position.x; // Calculate movement vector based on current position and the point
+        float moveY = yCoord - transform.position.y;
+
+        movement = new Vector3(moveX, moveY, 0); // Set values for movement vector
+
+        movement.Normalize(); // Normalize vector
+
+        if (Mathf.Abs(moveX) <= tolerance && Mathf.Abs(moveY) <= tolerance) // Once enemy reaches the desired patrol point within a given tolerance
         {
-            float moveX = xCoord - transform.position.x; // Calculate movement vector based on current position and the point
-            float moveY = yCoord - transform.position.y;
+            movingTowardsPoint1 = !movingTowardsPoint1; // Start heading towards the other point
+            float velocityHolder = patrolMoveVelocity;
+            patrolMoveVelocity = 0; // Stop moving
 
-            movement = new Vector3(moveX, moveY, 0); // Set values for movement vector
-
-            movement.Normalize(); // Normalize vector
-
-            if (Mathf.Abs(moveX) <= tolerance && Mathf.Abs(moveY) <= tolerance) // Once enemy reaches the desired patrol point within a given tolerance
+            if (!onePointPatrol)
             {
-                movingTowardsPoint1 = !movingTowardsPoint1; // Start heading towards the other point
-                float velocityHolder = patrolMoveVelocity;
-                patrolMoveVelocity = 0; // Stop moving
                 yield return new WaitForSeconds(atPointWaitTime); // Wait for a little bit based on the atPointWaitTime variable
                 patrolMoveVelocity = velocityHolder; // Start moving again
             }
-
-            rb.velocity = movement * patrolMoveVelocity; // Moves the enemy
-            float facingAngle = Mathf.Atan2(moveY, moveX) * Mathf.Rad2Deg - 90f; // Sets the angle the enemy is facing in degrees
-            anim.SetFloat("Facing", facingAngle); // Tell the animator which way the enemy is facing to set the appropriate sprites
         }
         
+
+        rb.velocity = movement * patrolMoveVelocity; // Moves the enemy
+        float facingAngle = Mathf.Atan2(moveY, moveX) * Mathf.Rad2Deg - 90f; // Sets the angle the enemy is facing in degrees
+        anim.SetFloat("Facing", facingAngle); // Tell the animator which way the enemy is facing to set the appropriate sprites
     }
+
 
     // Update is called once per frame
     void Update()
