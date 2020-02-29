1b0413f2c1ca88ed93f89aa9eee6f5814994a5bb
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-02-28 22:19:57 -0500

    Comments
     I really tried to be good but im so bad

--- Ruby/Assets/Scripts/CameraGrid.cs
+++ Ruby/Assets/Scripts/CameraGrid.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class CameraGrid : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
        
     }
-    void OnDrawGizmos()
+    void OnDrawGizmos() // A drawing utility
     {
-        Vector3 room = new Vector3(30, 20, 1);
+        Vector3 room = new Vector3(30, 20, 1); // Room size
         
-        Gizmos.color = Color.black;
+        Gizmos.color = Color.black; // Colors the drawing
         
-        for(int i = -100; i<100; i++)
+        for(int i = -100; i<100; i++) // Makes many boxes at different centers of room size, can be made bigger if needed.
         {
             for(int j = -100; j<100; j++)
             {
-                Vector3 center = new Vector3((i*30), ((j*20)-1), 0);
-                Gizmos.DrawWireCube(center, room);
+                Vector3 center = new Vector3((i*30), ((j*20)-1), 0); // Makes new center
+
+                Gizmos.DrawWireCube(center, room); // Draws a cube
             }
         }
     }
