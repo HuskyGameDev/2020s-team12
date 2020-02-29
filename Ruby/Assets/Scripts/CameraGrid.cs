5eb37b9b8389d6a6c965978111b8e3bed04f0e78
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-02-28 20:06:39 -0500

    ok dont private the camera
    oops

---
+++ Ruby/Assets/Scripts/CameraGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  Vector3 room =;
+    }
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.black;
+      //  Gizmos.DrawCube(transform.position,);
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
