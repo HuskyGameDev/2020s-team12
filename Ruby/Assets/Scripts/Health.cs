638de6cc73995e1d85fb0268916534153818bbd8
Jolo Vandenberg <jolov@mtu.edu>
2020-03-02 13:03:39 -0500

    Revert "Revert "Revert "Jolos Stuff"""
    This reverts commit d3a8f379efaf3451bcc191fd56127f8ed7d056f8.

--- Ruby/Assets/Scripts/Health.cs
+++ Ruby/Assets/Scripts/Health.cs
@@ -73,21 +73,21 @@ public class Health : MonoBehaviour
         rb.velocity = push; // Sets the gameObject's velocity to the resulting velocity vector
         if (tag == "Player")
         {
-            pmove.setMove(false); // If this is the player, keep them from moving
+            pmove.getMove().Equals(false); // If this is the player, keep them from moving
         }
         if (tag == "Enemy") // If this is the enemy, keep them from moving
         {
-            emove.setMove(false);
+            emove.getMove().Equals(false);
         }
         yield return new WaitForSeconds(knockTimer); // wait a bit
         rb.velocity = Vector3.zero; // Stop knockback
         if (tag == "Player")
         {
-            pmove.setMove(true); // Player can move again
+            pmove.getMove().Equals(true); // Player can move again
         }
         if (tag == "Enemy")
         {
-            emove.setMove(true);//enemy move again
+            emove.getMove().Equals(true);//enemy move again
         }
 
 
