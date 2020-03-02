e9a3c7687017f9eb987b125c4e5db2f02c560b29
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-02 18:32:07 -0500

    Added Portraits and Cutscene Scene

---
+++ Ruby/Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+    public Animator rubyAnimator;
+    RuntimeAnimatorController rubyAnim;
+    public PlayableDirector director;
+
+    void OnEnable()
+    {
+        rubyAnim = rubyAnimator.runtimeAnimatorController;
+        rubyAnimator.runtimeAnimatorController = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(director.state != PlayState.Playing && !fix)
+        {
+            fix = true;
+            rubyAnimator.runtimeAnimatorController = rubyAnim;
+        }
+    }
+}
