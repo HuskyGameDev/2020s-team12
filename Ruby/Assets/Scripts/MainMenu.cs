fd35c6c6e1dd7785515ef9b55d2565c56dea510d
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-01 21:39:52 -0500

    Title Screen 1

---
+++ Ruby/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Set in build settings, import title screen, then game. Check index.
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Ruby/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+
+
+    public void SetVolume(float volume)
+    {
+        audiomixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
