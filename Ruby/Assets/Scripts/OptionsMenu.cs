9bafaea9db095a2ed2f4194ba467f49763624d23
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-01 22:33:23 -0500

    Save Preferenes

--- Ruby/Assets/Scripts/OptionsMenu.cs
+++ Ruby/Assets/Scripts/OptionsMenu.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audiomixer;
+    public Slider volumeSlider;
+    public Toggle fullScreen;
 
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f); // Sets the volume slider at the correct location
+        audiomixer.SetFloat("Volume", PlayerPrefs.GetFloat("Volume")); // Sets the audio mixer to the correct volume
+
+        Screen.fullScreen = (PlayerPrefs.GetInt("Fullscreen") != 0); // Checks to see if it's true and sets it to true
+    }
 
     public void SetVolume(float volume)
     {
-        audiomixer.SetFloat("Volume", volume);
+        PlayerPrefs.SetFloat("Volume", volume); // Sets the player pref to the right audio
+        audiomixer.SetFloat("Volume", Mathf.Log10 (volume) * 20); // Sets float of audio mixer
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
-        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", (isFullscreen ? 1 : 0)); // Sets fullscreen boolean value in player preferences
+        Screen.fullScreen = isFullscreen; // Sets fullscreen
     }
 }
