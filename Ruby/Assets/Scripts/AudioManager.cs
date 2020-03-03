e2c0f58166085fa9c1d54e6dcb1d4a4cf8d3eb4a
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-03 15:41:27 -0500

    Audio Manager Rework
    :)

--- Ruby/Assets/Scripts/AudioManager.cs
+++ Ruby/Assets/Scripts/AudioManager.cs
@@ -1,11 +1,36 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
 using System;
+[System.Serializable]
+public class Sound
+{
+    // Sets up a default Sound object to access
+    public string Name;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+    [HideInInspector]
+    public AudioSource source;
+    public bool loop = false;
+    public bool playOnAwake = false;
+    public AudioMixerGroup audioGroup;
+
+    public void setSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.loop = loop;
+        source.outputAudioMixerGroup = audioGroup;
+    }
+}
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds; // Creates an array of sounds
-
     public static AudioManager instance;
 
     void Awake()
@@ -27,6 +52,7 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.audioGroup;
         }
     }
 
