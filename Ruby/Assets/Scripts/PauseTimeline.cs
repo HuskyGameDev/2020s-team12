fe1b5c7e5010377eeeccfe5a55fc126fa1998739
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-05 20:05:01 -0500

    Cutscene Manager Fix

---
+++ Ruby/Assets/Scripts/PauseTimeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PauseTimeline : MonoBehaviour
+{
+    public PlayableDirector director;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        director.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Submit") == 1)
+        {
+            director.Resume();
+        }
+    }
+}
--- Ruby/Assets/Scripts/TimelineManager.cs
+++ Ruby/Assets/Scripts/TimelineManager.cs
@@ -19,6 +19,7 @@ public class TimelineManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(director.state != PlayState.Playing && !fix)
         {
             fix = true;
