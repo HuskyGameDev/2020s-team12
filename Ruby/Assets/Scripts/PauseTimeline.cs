700ebf3a5589fd8241e8aa84db7e5989a215b7ba
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2020-03-05 20:25:36 -0500

    Update Dialogue Stuff

--- Ruby/Assets/Scripts/PauseTimeline.cs
+++ Ruby/Assets/Scripts/PauseTimeline.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class PauseTimeline : MonoBehaviour
 {
     public PlayableDirector director;
+    public Text dialogueText;
+    public Text speakerName;
+    public Image portraitSprite;
+    public Image dialogBox;
+
+    public Sprite[] portraits;
+
     // Start is called before the first frame update
     void OnEnable()
     {
@@ -18,6 +26,7 @@ public class PauseTimeline : MonoBehaviour
         if (Input.GetAxis("Submit") == 1)
         {
             director.Resume();
+            portraitSprite.sprite = portraits[0];
         }
     }
 }
